16f452cbbb33621bbf8a962697e39b10cdeba518
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-07-15 13:41:46 +0200

    Update semeval-latin-tokenization.sh

--- sbatch/semeval-latin-tokenization.sh
+++ sbatch/semeval-latin-tokenization.sh
@@ -1,6 +1,14 @@
+#dataset="$1/LSC/SemEval-Latin"
+#sampling=0
+#output="$2/LSC/SemEval-Latin"
+#tokenization_class="StandardTokenization"
+#python3 "src/tokenization.py" -d "${dataset}" -n "${sampling}" -o "${output}" -t "${tokenization_class}"
+
 dataset="$1/LSC/SemEval-Latin"
 sampling=0
 output="$2/LSC/SemEval-Latin"
-tokenization_class="StandardTokenization"
+tokenization_class="LatinSpacyTokenization"
+model='la_core_web_lg'
 
-python3 "src/tokenization.py" -d "${dataset}" -n "${sampling}" -o "${output}" -t "${tokenization_class}"
+pip install https://huggingface.co/latincy/la_core_web_lg/resolve/main/la_core_web_lg-any-py3-none-any.whl
+python3 "src/tokenization.py" -d "${dataset}" -n "${sampling}" -o "${output}" -t "${tokenization_class}" -m "${model}"
