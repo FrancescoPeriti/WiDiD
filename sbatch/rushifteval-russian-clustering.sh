e22423a8738047170335a23d008b1aa20f6210e8
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-07-15 17:41:30 +0200

    Update rushifteval-russian-clustering.sh

--- sbatch/rushifteval-russian-clustering.sh
+++ sbatch/rushifteval-russian-clustering.sh
@@ -8,7 +8,7 @@ layers=12
 declare -a embeddings_folders=("$1/LSC/GlossReader12-Russian" "$1/LSC/GlossReader23-Russian" "$1/LSC/GlossReader13-Russian")
 declare -a labels_folders=("$2/LSC/GlossReader12-Russian" "$2/LSC/GlossReader23-Russian" "$2/LSC/GlossReader13-Russian")
 declare -a dataset_folders=("$3/LSC/GlossReader12-Russian" "$3/LSC/GlossReader23-Russian" "$3/LSC/GlossReader13-Russian")
-declare -a algorithms=("app" "ap")
+declare -a algorithms=("app")
 declare -a models=("xlm-roberta-base" "bert-base-multilingual-cased" "DeepPavlov_rubert-base-cased")
 
 
@@ -16,8 +16,6 @@ for model in "${models[@]}"
 do
     python src/clustering.py -a "app_incremental" -e "$1/LSC/GlossReader12-Russian/${model}" -l "${layers}" -o "$2/LSC/GlossReader12-Russian/${model}" -t "$3/LSC/GlossReader12-Russian/targets_test.txt"
 done
-
-
 
 #for model in "${models[@]}"
 #do
