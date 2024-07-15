ed7dd0e0e210016fbb830c249303544582f8fe18
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-07-15 17:51:38 +0200

    Update semeval-swedish-clustering.sh

--- sbatch/semeval-swedish-clustering.sh
+++ sbatch/semeval-swedish-clustering.sh
@@ -8,7 +8,7 @@ labels_folder="$2/LSC/SemEval-Swedish"
 dataset_folder="$3/LSC/SemEval-Swedish"
 layers=12
 
-declare -a algorithms=("app" "ap")
+declare -a algorithms=("app")
 declare -a models=("xlm-roberta-base" "bert-base-multilingual-cased" "KB_bert-base-swedish-cased")
 
 for model in "${models[@]}"
